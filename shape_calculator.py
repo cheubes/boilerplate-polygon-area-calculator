f8082c8516916308902521d73debcf4617f4c5a4
cheubes <christophe@heubes.com>
2021-01-25 17:18:26 +0100

    Pass test_subclass, test_distinct_classes and test_square_is_square_and_rectangle.

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,11 @@
 class Rectangle:
 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
+class Square(Rectangle):
 
-class Square:
+    def __init__(self, side_length):
+        Rectangle.__init__(self, side_length, side_length)
